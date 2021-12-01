3923091a16f2d08a5625d85cff28cc157214ed53
Duncan Clarke <bunkford@gmail.com>
2021-12-01 08:15:45 -0500

    cleanup

--- aoc_2021_01.nim
+++ aoc_2021_01.nim
@@ -1,24 +1,17 @@
-import os, strutils
+import os, strutils, sequtils
 
-proc solve1(file: string): int = 
-  var old:int
-  for line in file.lines:
-    if line.parseInt() > old:
+proc solve1(data:seq[int]): int = 
+  for k in 1 .. data.high:
+    if data[k] > data[k - 1]:
       inc result
-    old = line.parseInt()
 
-proc solve2(file: string): int =
-  var data:seq[int]
-  for line in file.lines:
-    data.add(line.parseInt())
-
-  var old:int
-  for k in 0 .. data.len:
-    var val:int = data[k + 0] + data[k + 1] + data[k + 2]  
-    if val > old:
+proc solve2(data: seq[int]): int =
+  for k in 0 ..< (data.len - 3):
+    if data[k + 3] > data[k]:
       inc result
-    old = val 
 
-echo "Answer Part 1: ", solve1(getAppDir() / "aoc_2021_01.txt") - 1 # first one doesn't count, so we have to subtract it.
+let data = (getAppDir() / "aoc_2021_01.txt").lines.toSeq.map(parseInt)
 
-echo "Answer Part 2: ", solve2(getAppDir() / "aoc_2021_01.txt") - 1 # first one doesn't count, so we have to subtract it.
+echo "Answer Part 1: ", solve1(data)
+
+echo "Answer Part 2: ", solve2(data)
