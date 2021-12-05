9da128440e71aecb848ec0b6c97dd0946abac688
Duncan Clarke <bunkford@gmail.com>
2021-12-04 21:06:37 -0500

    Day 4 Part 2

--- aoc_2021_04.nim
+++ aoc_2021_04.nim
@@ -34,6 +34,44 @@ proc solve1(nums:seq[int], boards:seq[seq[int]]):int =
 
 
 
+proc solve2(nums:seq[int], boards:seq[seq[int]]):int = 
+  # write a checker to check boards for winning combinations adding 1 number each time
+  var num_to_call = 5
+  var called_nums:seq[int] = nums[0 .. 4]
+  var winning_boards:seq[int]
+
+  while winning_boards.len < boards.len:
+    for board_number, board in boards:
+      # check horizontals
+      for i in [0, 5, 10, 15, 20]:
+        let hline = board[i .. i + 4]
+        var found: int
+        for n in hline:
+          if n in called_nums:
+            found.inc()
+        if found == 5:
+          if board_number notin winning_boards: winning_boards.add(board_number)
+          break
+
+      # check verticals
+      for a in 0 .. 4:
+        var vline= @[board[a]]
+        for b in 1 .. 4:
+          vline.add(board[a + (5 * b)])
+        var found: int
+        for n in vline:
+          if n in called_nums:
+            found.inc()
+        if found == 5:
+          if board_number notin winning_boards: winning_boards.add(board_number)
+          break
+    
+    called_nums.add(nums[num_to_call])
+    num_to_call.inc()
+  
+  return boards[winning_boards[^1]].filterIt(it notin called_nums[0..^2]).foldl(a + b) * called_nums[^2]
+
+
 proc get_inputs(filename:string):(seq[int], seq[seq[int]]) =
   # add input to variables
   var line_num:int
@@ -59,3 +97,5 @@ proc get_inputs(filename:string):(seq[int], seq[seq[int]]) =
 let inputs = get_inputs(getAppDir() / "aoc_2021_04.txt")
 
 echo "Answer Part 1: ", solve1(inputs[0], inputs[1])
+
+echo "Answer Part 2: ", solve2(inputs[0], inputs[1])
