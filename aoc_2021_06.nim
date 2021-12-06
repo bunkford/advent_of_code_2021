5165192d476db0cdee04f6bc2e492c1e24896fad
Duncan Clarke <bunkford@gmail.com>
2021-12-06 12:29:48 -0500

    Day 6 Part 2

--- aoc_2021_06.nim
+++ aoc_2021_06.nim
@@ -30,9 +30,24 @@ proc solve1(data:seq[int], days:int):int =
   
   return sea.len
 
+  
+proc solve2(data:seq[int], days:int):int64 = 
+  var counts = newSeq[int64](9)
+  for i in 0 .. counts.high:
+    counts[i] = data.count(i)
 
-proc solve2(data:seq[int]):int = 
-  discard
+  for day in 1 .. days:
+    var new_counts = newSeq[int64](9)
+    for i in countdown(counts.high, 0):
+      if i == 0:
+        new_counts[8] = counts[0]
+        new_counts[6] += counts[0]
+      else:
+        new_counts[i-1] = counts[i]
+
+    counts = new_counts
+
+  result = counts.foldl(a + b)  
 
 
 let data = (getAppDir() / "aoc_2021_06.txt").lines.toSeq.mapIt(it.split(",").map(parseInt))[0]
@@ -40,4 +55,4 @@ let data = (getAppDir() / "aoc_2021_06.txt").lines.toSeq.mapIt(it.split(",").map
 
 echo "Answer Part 1: ", solve1(data, 80)
 
-echo "Answer Part 2: ", solve2(data)
+echo "Answer Part 2: ", solve2(data, 256)
