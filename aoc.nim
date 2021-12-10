046a4cc2bb12900b25c32d5f99186abee7ad553d
Duncan Clake <bunkford@gmail.com>
2021-12-10 10:52:18 -0500

    Added median function to aoc

--- aoc.nim
+++ aoc.nim
@@ -1,3 +1,5 @@
+import stats, strutils, algorithm
+
 proc transpose*[T](s: seq[seq[T]]): seq[seq[T]] =
   ## transposes seq of seqs 
   ## transpose(@[@[1, 2], @[3, 4], @[5, 6]]) == @[@[1, 3, 5], @[2, 4, 6]]
@@ -7,6 +9,14 @@ proc transpose*[T](s: seq[seq[T]]): seq[seq[T]] =
     for j in 0 .. s.high:
       result[i][j] = s[j][i]
 
+proc median*[T](s: seq[T]):int =
+  if s.high mod 2 == 1:
+    var m1 = s.sorted()[(s.len div 2) - 1]
+    var m2 = s.sorted()[(s.len div 2)]
+    # because there is no middle value, median is the mean of the two middle values
+    result = mean(@[m1, m2]).toInt
+  else:
+    result = parseFloat($(s.sorted()[(s.len div 2)])).toInt
 
 proc taxi_cab*(a:(int, int), b:(int, int)):int =
   # Takes coordinates and returns taxi cab geometry (shortest route)
