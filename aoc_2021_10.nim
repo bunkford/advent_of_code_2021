6194724b1c3cca3d951b4bef2b17257fc1b3cb4a
Duncan Clake <bunkford@gmail.com>
2021-12-10 10:10:31 -0500

    Day 10 Part 1

---
+++ aoc_2021_10.nim
@@ -0,0 +1,25 @@
+import os, strutils, sequtils, tables
+
+proc solve1(data:seq[string]): int = 
+  const brackets = {'(':')','[':']','{':'}','<':'>'}.toTable # matching brackets
+  const scores = {')':3,']':57,'}':1197,'>':25137}.toTable # scores for wrong closing bracket
+
+  for line in data:
+    var stack:string
+    for ch in line.toSeq:
+      if brackets.hasKey ch:
+        stack = brackets[ch] & stack
+      elif stack[0] == ch:
+        stack = stack[1..^1]
+      else:
+        result.inc scores[ch]
+        break # already found one wrong on this line
+
+proc solve2(data: seq[string]): int =
+  discard
+
+let data = (getAppDir() / "aoc_2021_10.txt").lines.toSeq
+
+echo "Answer Part 1: ", solve1(data)
+
+echo "Answer Part 2: ", solve2(data)
