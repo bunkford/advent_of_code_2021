08dadf0f821a5746b32f0ad8b85b81edd6c825e8
Duncan Clarke <bunkford@gmail.com>
2022-04-19 15:55:02 +0100

    Day 11 Part 2

--- aoc_2021_11.nim
+++ aoc_2021_11.nim
@@ -19,17 +19,24 @@ proc flash(data: var seq[seq[int]]): int =
             if data[coords[0]][coords[1]] > 9:
               result.inc flash(data)
 
-proc solve1(data: var seq[seq[int]], steps: int): int =
+proc solve1(data: seq[seq[int]], steps: int): int =
+  var d = data
   for step in 1 .. steps:
-    for y, line in data:
+    for y, line in d:
       for x, val in line:
-        data[y][x].inc
-    result.inc flash(data)
+        d[y][x].inc
+    result.inc flash(d)
 
-    echo "\nDay: ", step
-    for d in data:
-      echo d
+proc solve2(data: seq[seq[int]]): int =
+  var d = data
+  while d != repeat(repeat(0, 10), 10):
+    result.inc
+    for y, line in d:
+      for x, val in line:
+        d[y][x].inc
+    discard flash(d)
 
 var data = (getAppDir() / "aoc_2021_11.txt").lines.toSeq.mapIt(it.toSeq.mapIt(($it).parseInt))
 
 echo "Answer Part 1: ", solve1(data, 100) 
+echo "Answer Part 2: ", solve2(data) 
