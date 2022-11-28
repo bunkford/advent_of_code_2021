30d4dac51c1bf0a9d1f91c9f3e90329f9eca633a
Duncan Clarke <bunkford@gmail.com>
2022-11-28 12:05:22 -0500

    Day 14 Part 2

--- aoc_2021_14.nim
+++ aoc_2021_14.nim
@@ -13,7 +13,58 @@ for step in 1 .. 10:
     polymer = polymer[0..i] & insert & polymer[i+1..<polymer.len]
     i.inc 2
 
-  let leterFrequencies = polymer.toCountTable()
-  
+  let leterFrequencies = polymer.toCountTable()  
 
   echo "step ", step ,": ", leterFrequencies.largest[1] - leterFrequencies.smallest[1]
+
+
+# Part 2, had to run this on my mac, raspberry pi failed with overflow error on counttable
+
+polymer = data[0][0]
+
+var letter_count =  toCountTable(polymer)
+
+
+echo letter_count
+
+var table:CountTable[string]
+
+for i in 0..<polymer.len-1:
+  table.inc(polymer[i..i+1])
+
+echo table
+
+for i in 1 .. 39:
+  var new_table = table
+  for key, letter_to_insert in rules:
+    var left_side = key[0] & letter_to_insert
+    var right_side = letter_to_insert & key[1]
+    
+    new_table.inc(key, 0)
+    new_table.inc(left_side, 0)
+    new_table.inc(right_side, 0)
+
+    if key in table:
+      if table[key] > 0:
+        var val = table[key]
+
+        if letter_count.contains(letter_to_insert[0]) == false:
+          letter_count[letter_to_insert[0]] = val
+        else:
+          letter_count[letter_to_insert[0]] = letter_count[letter_to_insert[0]] + val
+        
+        new_table[key] = new_table[key] - val
+        new_table.inc(left_side, val)
+        new_table.inc(right_side, val)
+    
+  table = new_table
+
+echo letter_count.largest.val - letter_count.smallest.val
+
+
+
+
+
+
+
+ 
